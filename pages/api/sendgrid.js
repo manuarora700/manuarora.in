136a9f693409f96c0eacbe7706fd5b8e0cf2d867
Manu Arora <mannuarora700@gmail.com>
2021-03-20 20:20:47 +0530

    🔩Fix: Sendgrid email template

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -8,9 +8,47 @@ export default async (req, res) => {
     await sendgrid.send({
       to: req.body.email,
       from: "manuarorawork@gmail.com",
-      subject: "Greetings! Thankyou for contacting Manu",
-      text:
-        "Thank you for contacting me from my website.\nPlease reply to this mail with your requirements and let me know how I can help you with your project.\n\nBest Regards,\nManu Arora\nSoftware Developer\n+91, 9587738861\n",
+      subject: "Greetings! Thankyou for contacting me",
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+      </head>
+      
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>Hello, ${req.body.email} 👋</h3>
+              <div style="font-size: 16px;">
+              <p>Thankyou for contacting me through my website. I'm excited to work with you.</p>
+              <p><span class="spantext" style="font-weight: bold;">Reply</span> to this email with your requirements and I will get back to you.</p>
+              <p>Meanwhile, <a class="blog" href="https://manuarora.in/blog/" style="text-decoration: none;color: blue;font-weight: bold;">checkout my blog</a> to get more insights about my work.</p>
+              <p>Or simply, just say Hi :) <a href="https://wa.me/919587738861?text=Hi" target="__blank" class="whatsapp" style="text-decoration: none;font-weight: bold;background: #10B981;padding: 4px;border-radius: 5px;color: white;">WhatsApp</a></p>
+              <br>
+              </div>
+              <img src="https://manuarora.in/logo.png" class="logo-image" style="height: 50px;width: 50px;border-radius: 5px;overflow: hidden;">
+              <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Manu Arora<br>Software Developer<br>+91 9587738861</p>
+              <div class="footer-links" style="display: flex;justify-content: center;align-items: center;">
+                <a href="https://manuarora.in/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Website</a>
+                <a href="https://manuarora.in/blog/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Blog</a>
+                <a href="https://github.com/manuarora700/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">GitHub</a>
+                <a href="https://instagram.com/maninthere/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Instagram</a>
+                <a href="https://linkedin.com/in/manuarora28/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">LinkedIn</a>
+                <a href="https://twitter.com/mannupaaji/" style="text-decoration: none;margin: 8px;color: #9CA3AF;">Twitter</a>
+                
+              </div>
+              </div>
+      </body>
+      </html>`,
     });
   } catch (error) {
     // console.log(error);
