6e2b5176e2110c4121a45e6e1ec35676a97a3ea6
Muhammad Azhar <azharmuham13@hotmail.com>
2021-10-22 19:57:04 +0530

    enabled spotify

--- components/Footer.js
+++ components/Footer.js
@@ -17,7 +17,7 @@ const ExternalLink = ({ href, children }) => (
 export default function Footer() {
   return (
     <footer className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-8">
-      {/* <NowPlaying /> */}
+      <NowPlaying />
       <hr className="w-full mb-8 border-gray-200 border-1 dark:border-gray-800" />
       <div className="grid w-full max-w-2xl grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
         <div className="flex flex-col space-y-4">
