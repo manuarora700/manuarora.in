2cbcf284b990a1a7eea9e2334aa1a900f3dc89c0
Muhammad Azhar <azharmuham13@hotmail.com>
2021-10-22 17:53:17 +0530

    updated tweets page

--- pages/tweets.js
+++ pages/tweets.js
@@ -1,54 +1,55 @@
 import Container from "@/components/Container";
 import Tweet from "@/components/Tweet";
 import { getTweets } from "@/lib/twitter";
+import { appConfig } from "constants/app";
 
 export default function Tweets({ tweets }) {
   return (
     <Container
-      title="Tweets – Manu Arora"
+      title={`Tweets – ${appConfig.name}`}
       description="A collection of tweets that make me laugh, make me think and make me learn."
     >
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
           Tweets
         </h1>
-        <p className="text-gray-600 dark:text-gray-400 mb-4">
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
           Collection of my personal favourite tweets. 🕊
         </p>
-        <p className="text-gray-600 dark:text-gray-400 mb-4">
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
           Also, not to forget, built using the Twitter API V2 (Super easy, super
           fun). 🕊
         </p>
-        {tweets.map((tweet) => (
+        {/* {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
-        ))}
+        ))} */}
       </div>
     </Container>
   );
 }
 
-export async function getStaticProps() {
-  const tweets = await getTweets([
-    "1371103308425928708",
-    "826528907381739520",
-    "1308509070140162048",
-    "997895977179721728",
-    "1341090253864542208",
-    "1026872652290379776",
-    "1346113149112619016",
-    "1340107217970683906",
-    "992629481578745856",
-    "989142253468708864",
-    "1368058884837928970",
-    "1341962177272537089",
-    "1342869937841266688",
-    "1116362674319908864",
-    "1360610614490501126",
-    "1371103308425928708",
-  ]);
+// export async function getStaticProps() {
+//   const tweets = await getTweets([
+//     "1371103308425928708",
+//     "826528907381739520",
+//     "1308509070140162048",
+//     "997895977179721728",
+//     "1341090253864542208",
+//     "1026872652290379776",
+//     "1346113149112619016",
+//     "1340107217970683906",
+//     "992629481578745856",
+//     "989142253468708864",
+//     "1368058884837928970",
+//     "1341962177272537089",
+//     "1342869937841266688",
+//     "1116362674319908864",
+//     "1360610614490501126",
+//     "1371103308425928708",
+//   ]);
 
-  return { props: { tweets } };
-}
+//   return { props: { tweets } };
+// }
 
 // export default function Tweets({ tweets }) {
 //   return (
@@ -56,11 +57,11 @@ export async function getStaticProps() {
 //       title="Tweets – Manu Arora"
 //       description="A collection of tweets that make me laugh, make me think and make me learn."
 //     >
-//       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-//         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+//       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+//         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
 //           Tweets
 //         </h1>
-//         <p className="text-gray-600 dark:text-gray-400 mb-4">
+//         <p className="mb-4 text-gray-600 dark:text-gray-400">
 //           This is a collection of tweets I've enjoyed. I use Twitter quite a
 //           bit, so I wanted a place to publicly share what inspires me, makes me
 //           laugh, and makes me think.
@@ -68,7 +69,7 @@ export async function getStaticProps() {
 //         {/* {tweets.map((tweet) => (
 //           <Tweet key={tweet.id} {...tweet} />
 //         ))} */}
-//         <p className="text-gray-600 dark:text-gray-400 mb-4">Coming soon...</p>
+//         <p className="mb-4 text-gray-600 dark:text-gray-400">Coming soon...</p>
 //       </div>
 //     </Container>
 //   );
