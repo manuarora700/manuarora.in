2ac41deea134a86af7dde7fb0e791c06e2ae14c6
Muhammad Azhar <azharmuham13@hotmail.com>
2021-10-30 00:55:56 +0530

    changed favicons

--- pages/_document.js
+++ pages/_document.js
@@ -35,53 +35,53 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="/static/favicons/apple-icon-57x57.png"
+            href="/static/favicons/apple-touch-icon-57x57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="60x60"
-            href="/static/favicons/apple-icon-60x60.png"
+            href="/static/favicons/apple-touch-icon-60x60.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="/static/favicons/apple-icon-72x72.png"
+            href="/static/favicons/apple-touch-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/static/favicons/apple-icon-76x76.png"
+            href="/static/favicons/apple-touch-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="/static/favicons/apple-icon-114x114.png"
+            href="/static/favicons/apple-touch-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="/static/favicons/apple-icon-120x120.png"
+            href="/static/favicons/apple-touch-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="/static/favicons/apple-icon-144x144.png"
+            href="/static/favicons/apple-touch-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="/static/favicons/apple-icon-152x152.png"
+            href="/static/favicons/apple-touch-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicons/apple-icon-180x180.png"
+            href="/static/favicons/apple-touch-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/static/favicons/android-icon-192x192.png"
+            href="/static/favicons/android-chrome-192x192.png"
           />
           <link
             rel="icon"
@@ -101,12 +101,17 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/static/favicons/manifest.json" />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          {/* <link rel="manifest" href="/static/favicons/manifest.json" /> */}
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta
             name="msapplication-TileImage"
-            content="/static/favicons/ms-icon-144x144.png"
+            content="/static/favicons/mstile-144x144.png"
           />
+          {/* <meta
+            name="msapplication-TileImage"
+            content="/static/favicons/ms-icon-144x144.png"
+          /> */}
           <meta name="theme-color" content="#ffffff" />
           {/* TODO */}
           {/* <meta
@@ -114,7 +119,7 @@ class MyDocument extends Document {
             name="google-site-verification"
           /> */}
         </Head>
-        <body className="bg-white dark:bg-black text-white dark:text-black">
+        <body className="text-white bg-white dark:bg-black dark:text-black">
           <Main />
           <NextScript />
         </body>
