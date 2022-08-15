a28f7c7806b0f5d90304cf985c5a62ac8e0864bf
Manu Arora <manuarorawork@gmail.com>
2022-08-15 23:55:38 +0530

    Add: CC for emails

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -8,6 +8,7 @@ export default async (req, res) => {
     await sendgrid.send({
       to: req.body.email,
       from: "manuarorawork@gmail.com",
+      cc: "manuarorawork@gmail.com",
       subject: "Greetings! Thankyou for contacting me",
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
