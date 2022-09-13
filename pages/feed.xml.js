223915607f8c6386ff46236e3103e3774de3aa9e
Manu Arora <manuarorawork@gmail.com>
2022-09-13 20:46:56 +0530

    Add: RSS feed for blogs

---
+++ pages/feed.xml.js
@@ -0,0 +1,39 @@
+import { getAllFilesFrontMatter } from "@/lib/mdx";
+import RSS from "rss";
+
+export async function getServerSideProps({ res }) {
+  const feed = new RSS({
+    title: "Manu Arora",
+    site_url: "https://manuarora.in",
+    feed_url: "https://leerob.io/feed.xml",
+  });
+
+  const posts = await getAllFilesFrontMatter("blog");
+  const filteredBlogPosts = posts.sort(
+    (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+  );
+  filteredBlogPosts.map((post) => {
+    feed.item({
+      title: post.title,
+      url: `https://manuarora.in/blog/${post.slug}`,
+      date: post.publishedAt,
+      description: post.summary,
+    });
+  });
+
+  res.setHeader("Content-Type", "text/xml");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=1200, stale-while-revalidate=600"
+  );
+  res.write(feed.xml({ indent: true }));
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function RSSFeed() {
+  return null;
+}
