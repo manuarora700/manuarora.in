3be59f69f435a51ac254cb560c24b28f1f11fa4c
Manu Arora <manuarorawork@gmail.com>
2023-01-18 14:15:53 +0530

    add: margin

--- components/PreviewContainer/PreviewContainer.js
+++ components/PreviewContainer/PreviewContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const PreviewContainer = ({ children }) => {
   return (
-    <div className="min-h-96 w-full rounded-2xl flex flex-row items-center justify-center bg-grid-slate-100/40 border border-slate-100 dark:bg-grid-slate-800 dark:border-slate-800 py-10 px-4">
+    <div className="min-h-96 w-full rounded-2xl flex flex-row items-center justify-center bg-grid-slate-100/40 border border-slate-100 dark:bg-grid-slate-800 dark:border-slate-800 py-10 px-4 mb-10">
       <div className="relative z-10">{children}</div>
     </div>
   );
