6f92bd62569f752cf448de99093b06b82b89c2d2
Manu Arora <manuarorawork@gmail.com>
2023-12-19 10:02:27 +0530

    add: derek shadow

--- constants/boxshadows/cards.js
+++ constants/boxshadows/cards.js
@@ -20,6 +20,16 @@ const cards = [
     shadow: "shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]",
     vanillaCSS: `box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;`,
   },
+  {
+    name: "Derek",
+    shadow: `shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]`,
+    vanillaCSS: `box-shadow: 0px 0px 0px 1px rgb(0 0 0 / 0.06),
+    0px 1px 1px -0.5px rgb(0 0 0 / 0.06),
+    0px 3px 3px -1.5px rgb(0 0 0 / 0.06), 
+    0px 6px 6px -3px rgb(0 0 0 / 0.06),
+    0px 12px 12px -6px rgb(0 0 0 / 0.06),
+    0px 24px 24px -12px rgb(0 0 0 / 0.06);`,
+  },
 
   {
     name: "Sprightly",
